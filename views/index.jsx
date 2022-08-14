0345b7750fe43b5e65fdfdce17e861076105e08e
Thomas McCarthy <tommccarthyguitar@gmail.com>
2022-08-14 08:43:52 -0500

    breads app up to day 5

---
+++ views/index.jsx
@@ -0,0 +1,31 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index({ breads }) {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      <div className="newButton">
+        <a href="/breads/new"><button>Add a new bread</button></a>
+      </div>
+
+      <ul>
+        {
+          breads.map((bread, index) => {
+            return (
+              <li key={index}>
+                <a href={`/breads/${index}`}>
+                  {bread.name}
+                </a>
+              </li>
+            )
+          })
+        }
+
+      </ul>
+    </Default>
+  )
+}
+
+
+module.exports = Index
